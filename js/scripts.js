45a1487107db9dd0c8c3ee07a95ed103f5adf1fe
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-03 18:48:14 -0700

    Add vanilla js to show hidden div

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,31 @@ document.addEventListener("DOMContentLoaded", function(){
     const split = Number(document.getElementById('split').value);
     const tip = bill * ((percent/100).toFixed(2));
     const total = (bill + tip).toFixed(2);
+    const splitTip = (tip / split).toFixed(2);
     const splitTotal = ((bill + tip) / split).toFixed(2);
-
-    document.getElementById('tip').innerHTML = `Tip: ${'$'} ${tip}`;
-    document.getElementById('total').innerHTML = `Total: ${'$'} ${total}`;
-    if (split > 1) {
-      document.getElementById('splitTotal').innerHTML = `${'$'} ${splitTotal} each`;
-    }
+    
+    document.getElementById('tip').innerHTML = `${'$'} ${tip}`;
+    document.getElementById('total').innerHTML = `${'$'} ${total}`;
+    document.getElementById('splitTip').innerHTML = `${'$'} ${splitTip}`;
+    document.getElementById('splitTotal').innerHTML = `${'$'} ${splitTotal}`;
   }
 });
+
+// Show an element
+function showResults(elem) {
+	elem.classList.add('is-visible');
+};
+
+document.addEventListener('click', function (event) {
+	if (!event.target.classList.contains('toggle')) return;
+
+  event.preventDefault();
+
+	// Get the content
+	var content = document.querySelector(event.target.hash);
+	if (!content) return;
+
+	// Show the content
+	showResults(content);
+
+}, false);
